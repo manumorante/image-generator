3df3682f105557b37bc2c7265076d6994304bed9
Manu Morante <manu@estadologico.com>
2022-09-23 23:35:27 +0200

    Simple generation from to with button

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,40 @@
-const packageJson = require('../package.json')
+import { toJpeg } from 'html-to-image'
+
 export default function Index() {
+  function generate({ fromElem, toContainer }) {
+    toJpeg(fromElem)
+      .then(function (dataUrl) {
+        var img = new Image()
+        img.src = dataUrl
+        toContainer.innerHTML = img.outerHTML
+      })
+      .catch(function (error) {
+        console.error('oops, something went wrong!', error)
+      })
+  }
+
+  const handleClick = () => {
+    const fromElem = document.getElementById('fromElem')
+    const toContainer = document.getElementById('toContainer')
+
+    if (fromElem && toContainer) {
+      generate({ fromElem, toContainer })
+    } else {
+      console.error('error')
+    }
+  }
+
   return (
-    <div className='p-6'>
-      <h1 className='font-bold mb-3'>Next.js + Tailwind CSS</h1>
-      {/* Mostrar el archivo package.json en formato texto: */}
-      <pre>{JSON.stringify(packageJson, null, 2)}</pre>
+    <div className='p-6 flex flex-col items-start gap-4'>
+      <div
+        id='fromElem'
+        className='w-[600px] h-[315px] flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+        <div className='text-white font-extrabold text-2xl'>Manu Morante</div>
+      </div>
+
+      <div id='toContainer'>...</div>
+
+      <button onClick={handleClick}>Generate</button>
     </div>
   )
 }
